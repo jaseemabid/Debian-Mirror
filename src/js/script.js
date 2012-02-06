389f81766f520451e422ffc7ed07c93ce7f61b1d
Jaseem Abid <jaseemabid@gmail.com>
2012-02-07 00:33:27 +0530

    Moving JS to script.js, scrollspy fixed

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+/*
+	Main script for the Debian Mirror website.
+	Author : Jaseem Abid
+*/
+
+;(function($, window){
+	$(document).ready(function(){
+		$('#navbar').scrollspy();	
+	});
+})(jQuery,this);
