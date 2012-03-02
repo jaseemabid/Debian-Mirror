23110da08cea2ebfccb2e2195abf45defcf52d57
Jaseem Abid <jaseemabid@gmail.com>
2012-03-02 20:56:16 +0530

    Fixing timeago, typography

--- src/js/script.js
+++ src/js/script.js
@@ -9,5 +9,9 @@
 		$('div#screenshots').carousel({
 			interval: 2000
 		});
+		$.getJSON('api/api.php?lastupdate',function(data){
+			$('div#last-update time').attr('datetime',data.date);
+			$("time.timeago").timeago();
+		});
 	});
 })(jQuery,this);
